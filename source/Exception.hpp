2d29d075ffba67d8803229d18e57ae267e77e747
EricMiddleton1 <ericm@iastate.edu>
2016-06-02 01:45:52 -0500

    Added exception class and error codes

---
+++ source/Exception.hpp
@@ -0,0 +1,24 @@
+#ifndef EXCEPTION_HPP
+#define EXCEPTION_HPP
+
+#include <exception>
+#include <string>
+#include <cstdint>
+
+class Exception : public std::exception {
+public:
+	//constructor
+	Exception(uint16_t errorCode, const std::string& msg);
+
+	//returns the error message
+	virtual const char* what() const noexcept;
+
+	uint16_t getErrorCode() const noexcept;
+
+private:
+	uint16_t errorCode;
+	std::string msg;
+};
+
+
+#endif //EXCEPTION_HPP
