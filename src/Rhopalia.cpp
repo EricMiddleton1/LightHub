49e7535b6adc8bc14b541de3dc6ca298b00c11da
EricMiddleton1 <ericm@iastate.edu>
2016-07-18 02:30:00 -0500

    Updated Rhopalia controller system

--- src/Rhopalia.cpp
+++ src/Rhopalia.cpp
@@ -1,4 +1,10 @@
 #include "Rhopalia.hpp"
 
 
+Rhopalia::Rhopalia()
+	:	hub(SEND_PORT, RECV_PORT, DISCOVER_METHOD) {
+	//TODO: Read these values from a config file
+}
 
+Rhopalia::~Rhopalia() {
+}
--- src/Rhopalia.hpp
+++ src/Rhopalia.hpp
@@ -10,13 +10,32 @@
 class Rhopalia
 {
 public:
+	enum ListenerType_e {
+		NODE_DISCOVER
+	};
+
 	Rhopalia();
 	~Rhopalia();
 
-
+	template <class T>
+	void addListener(ListenerType_e listenType, T slot) {
+		if(listenType == NODE_DISCOVER) {
+			hub.addListener(LightHub::NODE_DISCOVER, slot);
+		}
+		else {
+			std::cout << "[Error] Rhopalia::addListener: Invalid listener type"
+				<< std::endl;
+		}
+	}
 
 
 private:
+	//TODO: Read these values from a config file
+	static const uint16_t SEND_PORT = 54923;
+	static const uint16_t RECV_PORT = 54924;
+	static const LightHub::DiscoveryMethod_e DISCOVER_METHOD =
+		LightHub::BROADCAST;
+
 	LightHub hub;
 	std::vector<ILightEffect> lightEffects;
 };
