ae1843b35e3cbe57cb77da1098d445c7ee431399
Eric Middleton <eric@localhost.localdomain>
2017-02-10 01:57:21 -0600

    Moved in-progress stuff to a separate folder

--- src/SoundColor.cpp
+++ src/SoundColor.cpp
@@ -7,6 +7,7 @@
 
 #include <cstdio> //For printf
 
+using namespace std;
 
 std::string SoundColorSettings::toString() {
 	std::stringstream ss;
@@ -121,7 +122,7 @@ void SoundColor::cbSpectrum(SpectrumAnalyzer*,
 
 	//Render color for left, right channels
 	renderColor(left, *leftSpectrum.get());
-	renderColor(right, *rightSpectrum.get());
+	//renderColor(right, *rightSpectrum.get());
 
 	std::cout << left.c.toString() << std::endl;
 
@@ -134,10 +135,25 @@ void SoundColor::renderColor(ColorChannel& prevColor, Spectrum& spectrum) {
 	double r = 0., g = 0., b = 0.;
 	size_t binCount = spectrum.getBinCount();
 
-	double curAvg = spectrum.getAverageEnergyDB() + settings.noiseFloor;
+	//double curAvg = spectrum.getAverageEnergyDB() + settings.noiseFloor;
 
-	if(curAvg < 0)
-		curAvg = 0;
+	//if(curAvg < 0)
+		//curAvg = 0;
+
+	double absFloor = std::pow(10., -100);
+	double absBoost = std::pow(10., settings.bassBoost/20.);
+
+	cout << "[Info] Noise Floor: " << absFloor << '\n';
+	cout << "[Info] Bass Boost: " << absBoost << '\n';
+
+	double curAvg = spectrum.getAverageEnergy();
+
+	cout << "[Info] Average energy: " << curAvg << endl;
+
+	if(curAvg < absFloor)
+		curAvg = absFloor;
+	
+
 
 	//Loosely track the average DB
 	/*if(prevColor.avg > curAvg) {
@@ -153,9 +169,13 @@ void SoundColor::renderColor(ColorChannel& prevColor, Spectrum& spectrum) {
 	}*/
 	prevColor.avg = prevColor.avg*settings.avgFilterStrength
 		+ curAvg*(1. - settings.avgFilterStrength);
+	
+
 
 	//Scale to be applied to each bin
-	double scale = 1. / settings.dbScaler;
+	//double scale = 1. / settings.dbScaler;
+	double scale = 500;
+
 
 	for(unsigned int i = 0; i < binCount; ++i) {
 		FrequencyBin& bin = spectrum.getByIndex(i);
@@ -168,38 +188,57 @@ void SoundColor::renderColor(ColorChannel& prevColor, Spectrum& spectrum) {
 			Color c = frequencyColors[i];
 			//Color c = Color::HSV(240.f * i / (binCount - 1), 1.f, 1.f);
 			double db = bin.getEnergyDB();
+			double energy = bin.getEnergy();
 
 			//Bass boost
-			if(f <= settings.bassFreq)
-				db += settings.bassBoost;
+			//if(f <= settings.bassFreq)
+				//db += settings.bassBoost;
 
 			//Trebble boost
-			if(f >= settings.trebbleFreq)
-				db += settings.trebbleBoost;
+			//if(f >= settings.trebbleFreq)
+				//db += settings.trebbleBoost;
+
+			if(f <= settings.bassFreq)
+				energy *= absBoost;
+
+			energy -= prevColor.avg;
+			if(energy < 0)
+				energy = 0;
 
 			//Raise by noise floor, subtract loosly-tracking average
-			db += settings.noiseFloor;// - prevColor.avg;
+			//db += settings.noiseFloor;// - prevColor.avg;
 
 			//Reject anything below the average
-			if(db <= prevColor.avg)
-				continue;
+			//if(db <= prevColor.avg)
+				//continue;
 
 			//Scale partially based on average level
-			db *= settings.dbFactor;
-			db += settings.avgFactor*prevColor.avg;
+			//db *= settings.dbFactor;
+			//db += settings.avgFactor*prevColor.avg;
 
 			//Add weighted color to running color average
-			r += db * c.getRed();
-			g += db * c.getGreen();
-			b += db * c.getBlue();
+			r += energy * c.getRed();
+			g += energy * c.getGreen();
+			b += energy * c.getBlue();
 		}
 	}
 
 	//Scale color
-	r *= scale;
-	g *= scale;
-	b *= scale;
+	r *= r*scale;
+	g *= g*scale;
+	b *= b*scale;
 
+	//Convert to DB
+	r = 20.*std::log10(r);
+	if(r < 0)
+		r = 0;
+	g = 20.*std::log10(g);
+	if(g < 0)
+		g = 0;
+	b = 20.*std::log10(b);
+	if(b < 0)
+		b = 0;
+	
 	//Compute largest component
 	double largest = std::max(r, std::max(g, b));
 
